dcda7874e0ad9b690047767d3733c27c8d1a0a3d
Leticia <leerogenski@gmail.com>
2024-07-24 21:10:48 -0300

    feed: atualização

--- jogo.js
+++ jogo.js
@@ -2,46 +2,111 @@ const prompt = require("prompt-sync")();
 
 const jogos = []
 
-const criar = () => {
-    const nome = prompt("Nome do Jogo: ")
-    const ano_lancamento = prompt("Ano de lançamento: ")
-    const duracao = prompt("Duração média em horas: ")
+const validarIndice = indice => indice => 0 && indice < jogos.length
+
+const modelo = () => {
+    const nome = prompt("Nome do jogo: ")
+    const anoLancamento = prompt("Ano de lançamento: ")
+    const duracao = prompt("Duração média: ")
     const preco = prompt("Preço: ")
-    const estudio = prompt("Qual o estúdio do jogo? ")
-    const sequencia = prompt("Qual a sequencia do jogo? ")
+    const estudio = prompt("Estúdio de criação: ")
+    let sequencia = -1
+    if(listar()) {
+        sequencia = prompt("Qual é a sequêcia do jogo? Digite 0 se não houver. ") -1;
+    }
 
     if(
         nome != "" &&
-        ano_lancamento >= 1962 && ano_lancamento <= 2024 &&
+        anoLancamento >= 1962 && anoLancamento <= 2024 &&
         duracao > 0 &&
         preco == 0 &&
         estudio != "" &&
-        ((sequencia > 0 && sequencia < jogos.length) || jogos.length ==0)
-
+        ((sequencia >= -1 && sequencia < jogos.length) || jogos.length == 0)
     ) {
-        jogos.push({
-            nome, ano_lancamento, duracao, preco, estudio, sequencia
-        })
-        console.log("Jogo cadastrado com sucesso! ")
+
+    return {
+        nome,
+        anoLancamento,
+        duracao,
+        preco,
+        estudio,
+        sequencia,
+    };
     } else {
-        console.log("Dados inválidos! ")
+        console.log("Dados inválidos. ")
     }
 };
+const criar = () => {
+    const jogo = modelo()
 
-const leitura = () => {
-    if(jogos.length == 0 ) {
-        console.log("Nenhum jogo encontrado")
+    if(jogo != undefined) {
+    jogos.push(jogo)
+    console.log("Jogo cadastrado com sucesso! ")
+}
+};
+
+const listar = () => {
+    if(jogos.length == 0) {
+        console.log("Nenhum jogo cadastrado. ")
+        return false
     } else {
         jogos.forEach((jogo, indice) => {
             console.log(`
-                ${indice + 1}.
-                Nome: ${jogo.nome}
-                Ano de Lancamento: ${jogo.ano_lancamento}
-                Duracao: ${jogo.duracao}
-                Preco: ${jogo.preco}
-                Estudio: ${jogo.estudio}
-                Sequencia: ${jogo.sequencia}
+            ${indice + 1}.
+            Nome: ${jogo.nome}
+            Ano de Lançamento: ${jogo.anoLancamento}
+            Duração: ${jogo.duracao}
+            Preço: ${jogo.preco}
+            Estúdio: ${jogo.estudio}
+            Sequência: ${jogo.sequencia}
             `)
         })
+
+        return true
     }
+}
+
+const atualizar = () => {
+    if(!listar()) {
+        return
+    }
+
+    const indice = prompt("Qual o índice que deseja atualizar? ") -1;
+
+    const jogo = modelo()
+
+    if(
+        jogo != undefined &&
+        indice >= 0 &&
+        indice < jogos.length
+) {
+    jogos[indice] = jogo
+
+    console.log("Jogo atualizado com sucesso! ")
+
+} else {
+    console.log("Indíce inválido. ")
+}
+};
+
+const remover = () => {
+    if(!listar()) {
+        return
+    }
+
+    const indice = prompt("Qual o índice que deseja remover? ") -1;
+
+    if(validarIndice(indice)) {
+        jogos.slice(indice, 1)
+        console.log("Jogo removido com sucesso! ")
+    } else {
+        console.log("Falha na remoção. ")
+    }
+};
+
+module.exports = {
+    criar, 
+    atualizar, 
+    listar,
+    remover
 }
